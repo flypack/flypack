d1eed8cc54c663b89563a5b3d37808d2ffd4f1fe
Rozinko Roman <r.rozinko@gmail.com>
2018-01-31 18:36:07 +0300

    add fly\database\QueryStarter class

---
+++ src/database/QueryStarter.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * QueryStarter
+ *
+ * @copyright Copyright (c) 2017 Roman Rozinko
+ * @license   MIT License
+ *
+ * @author    Roman Rozinko <r.rozinko@gmail.com>
+ * @since     0.3
+ */
+
+namespace fly\database;
+
+class QueryStarter
+{
+
+    // For QueryBuilder Class
+
+    protected $queryType = FALSE;
+    protected $resultFormat = FALSE;
+
+    protected $select = [];
+    protected $from = [];
+    protected $where = [];
+    protected $orderBy = [];
+    protected $offset = 0;
+    protected $limit = 0;
+
+    protected $relations = [];
+
+    // For QueryPreparer Class
+
+    protected $prepared = [];
+    protected $preparedSQL = '';
+    protected $preparedParams = [];
+    protected $preparedParamNumber = 0;
+
+    // For QueryExecuter Class
+
+    protected $executed = [
+        'main' => [],
+        'relations' => [],
+    ];
+
+    // For QueryParser Class
+
+    protected $parsed = [];
+
+}
