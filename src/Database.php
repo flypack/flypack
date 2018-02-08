65e3205e0c95af084aef343e8bfbbde8810f4938
Rozinko Roman <r.rozinko@gmail.com>
2018-02-08 11:07:31 +0300

    add fly\Database class

---
+++ src/Database.php
@@ -0,0 +1,286 @@
+<?php
+
+/**
+ * Database
+ *
+ * @copyright Copyright (c) 2017 Roman Rozinko
+ * @license   MIT License
+ *
+ * @author    Roman Rozinko <r.rozinko@gmail.com>
+ * @since     0.3
+ */
+
+namespace fly;
+
+use fly\database\QueryMain;
+use fly\helpers\ArrayHelper;
+
+class Database
+{
+    /**
+     * @var $pdo \PDO
+     */
+    private static $pdo;
+
+    /**
+     * @var array $config  array pdo connect config
+     * @var array $default default config
+     * @var array $opt     array pdo connect options
+     */
+    private static $config;
+    private static $default = [
+        'type' => 'mysql',
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+    ];
+    private static $opt;
+
+    /**
+     * Validate database connection type
+     *
+     * @param string $type
+     *
+     * @return bool
+     */
+    private static function isConfigTypeValid($type)
+    {
+        if (!in_array($type, ['mysql'])) {
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Validate database connection hostname
+     *
+     * @param string $hostname
+     *
+     * @return bool
+     */
+    private static function isConfigHostnameValid($hostname)
+    {
+        if (!preg_match('/^[-a-z0-9.:\/]+$/i', $hostname)) {
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Validate database connection database name
+     *
+     * @param string $database
+     *
+     * @return bool
+     */
+    private static function isConfigDatabaseValid($database)
+    {
+        if (!preg_match('/^[-_a-z0-9]+$/i', $database)) {
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Validate database connection username
+     *
+     * @param string $username
+     *
+     * @return bool
+     */
+    private static function isConfigUsernameValid($username)
+    {
+        if (!preg_match('/^[-_a-z0-9]+$/i', $username)) {
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Validate database connection charset
+     *
+     * TODO: add more charsets
+     *
+     * @param string $type
+     *
+     * @return bool
+     */
+    private static function isConfigCharsetValid($type)
+    {
+        if (!in_array($type, ['utf8'])) {
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Set connection config
+     *
+     * @param array $data
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    private static function setConfig($data)
+    {
+        // Set type to config
+        if (isset($data['type'])) {
+            if (self::isConfigTypeValid($data['type'])) {
+                self::$config['type'] = $data['type'];
+            } else {
+                throw new \Exception('fly\Database: Invalid connection type');
+            }
+        } else {
+            self::$config['type'] = self::$default['type'];
+        }
+
+        // Set hostname to config
+        if (isset($data['hostname'])) {
+            if (self::isConfigHostnameValid($data['hostname'])) {
+                self::$config['hostname'] = $data['hostname'];
+            } else {
+                throw new \Exception('fly\Database: Invalid connection hostname');
+            }
+        } else {
+            self::$config['hostname'] = self::$default['hostname'];
+        }
+
+        // Set database to config
+        if (isset($data['database'])) {
+            if (self::isConfigDatabaseValid($data['database'])) {
+                self::$config['database'] = $data['database'];
+            } else {
+                throw new \Exception('fly\Database: Invalid connection database');
+            }
+        } else {
+            return FALSE;
+        }
+
+        // Set username to config
+        if (isset($data['username'])) {
+            if (self::isConfigUsernameValid($data['username'])) {
+                self::$config['username'] = $data['username'];
+            } else {
+                throw new \Exception('fly\Database: Invalid connection username');
+            }
+        } else {
+            self::$config['username'] = self::$default['username'];
+        }
+
+        // Set password to config
+        if (isset($data['password'])) {
+            self::$config['password'] = $data['password'];
+        } else {
+            self::$config['password'] = self::$default['password'];
+        }
+
+        // Set charset to config
+        if (isset($data['charset'])) {
+            if (self::isConfigCharsetValid($data['charset'])) {
+                self::$config['charset'] = $data['charset'];
+            } else {
+                throw new \Exception('fly\Database: Invalid connection charset');
+            }
+        } else {
+            self::$config['charset'] = self::$default['charset'];
+        }
+
+        // Set dsn
+        self::$config['dsn'] = self::$config['type'] . ':host=' . self::$config['hostname'] . ';dbname=' . self::$config['database'] . ';charset=' . self::$config['charset'];
+
+        return TRUE;
+    }
+
+    /**
+     * Set connection options
+     *
+     * @param array $opt
+     *
+     * @return bool
+     */
+    private static function setOptions($opt)
+    {
+        self::$opt = $opt;
+
+        return TRUE;
+    }
+
+    /**
+     * Connect to database
+     *
+     * @param array $config
+     * @param array $opt
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    public static function Connect($config, $opt = [])
+    {
+        if (!ArrayHelper::isArray($config) || !count($config)) {
+            // Config is not an array or empty
+            throw new \Exception('fly\Database: Expects parameter 1 to be a valid config array');
+        }
+
+        // set config
+        if (!self::setConfig($config)) {
+            // config is not valid
+            throw new \Exception('fly\Database: Invalid config array');
+        }
+
+        // set options
+        self::setOptions($opt);
+
+        // connect
+        self::$pdo = new \PDO(self::$config['dsn'], self::$config['username'], self::$config['password'], self::$opt);
+
+        if (!self::$pdo) {
+            // pdo connect error
+            // TODO: add error info
+            throw new \Exception('fly\Database: PDO connect error');
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Close connection
+     */
+    public static function Close()
+    {
+        self::$pdo = null;
+    }
+
+    /**
+     * Execute SQL query
+     *
+     * @param string $sql
+     * @param array  $params
+     *
+     * @return mixed
+     */
+    public static function SQL($sql, $params = [])
+    {
+        // PDO prepare SQL
+        $stmt = self::$pdo->prepare($sql);
+
+        // Execute SQL with params
+        $stmt->execute($params);
+
+        // Fetch to array
+        $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    public static function Query()
+    {
+        return new QueryMain();
+    }
+
+}
