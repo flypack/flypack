ca556393c4301b1820c765cd74a9fff677ae75ae
Rozinko Roman <r.rozinko@gmail.com>
2018-03-06 14:22:24 +0300

    add tests for INSERT-VALUES prepare functions

--- tests/database/QueryMainTest.php
+++ tests/database/QueryMainTest.php
@@ -23,6 +23,17 @@ class _QueryMain extends QueryMain
         return $this->$name;
     }
 
+    /**
+     * @return $this
+     * @throws \Exception
+     */
+    public function prepare()
+    {
+        parent::prepare();
+
+        return $this;
+    }
+
 }
 
 class QueryBuilderTest extends TestCase
@@ -83,6 +94,54 @@ class QueryBuilderTest extends TestCase
             ['key' => 456],
             ['key' => 789]
         ], $class->getProtectedVar('insertValues'));
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testPrepareInsertValues()
+    {
+        $class = new _QueryMain();
+        $class->insert()->into('table_1')->values([
+            ['key' => 123],
+            ['key' => 456],
+            ['key' => 789]
+        ])->prepare();
+
+        $this->assertArrayHasKey('INSERT', $class->getProtectedVar('prepared'));
+        $this->assertArrayHasKey('INTO', $class->getProtectedVar('prepared'));
+
+        $this->assertEquals('INSERT', $class->getProtectedVar('prepared')['INSERT']);
+        $this->assertEquals('INTO `table_1`', $class->getProtectedVar('prepared')['INTO']);
+
+        $this->assertArrayHasKey('VALUES', $class->getProtectedVar('prepared'));
+        $this->assertArrayHasKey('0', $class->getProtectedVar('prepared')['VALUES']);
+        $this->assertArrayHasKey('1', $class->getProtectedVar('prepared')['VALUES']);
+        $this->assertArrayHasKey('2', $class->getProtectedVar('prepared')['VALUES']);
+
+        $this->assertEquals('(`key`) VALUES (:value1)', $class->getProtectedVar('prepared')['VALUES'][0]);
+        $this->assertEquals('(`key`) VALUES (:value1)', $class->getProtectedVar('prepared')['VALUES'][1]);
+        $this->assertEquals('(`key`) VALUES (:value1)', $class->getProtectedVar('prepared')['VALUES'][2]);
+
+        $this->assertArrayHasKey('0', $class->getProtectedVar('preparedParams'));
+        $this->assertArrayHasKey('1', $class->getProtectedVar('preparedParams'));
+        $this->assertArrayHasKey('2', $class->getProtectedVar('preparedParams'));
+
+        $this->assertArrayHasKey(':value1', $class->getProtectedVar('preparedParams')[0]);
+        $this->assertArrayHasKey(':value1', $class->getProtectedVar('preparedParams')[1]);
+        $this->assertArrayHasKey(':value1', $class->getProtectedVar('preparedParams')[2]);
+
+        $this->assertEquals('123', $class->getProtectedVar('preparedParams')[0][':value1']);
+        $this->assertEquals('456', $class->getProtectedVar('preparedParams')[1][':value1']);
+        $this->assertEquals('789', $class->getProtectedVar('preparedParams')[2][':value1']);
+
+        $this->assertArrayHasKey('0', $class->getProtectedVar('preparedSQL'));
+        $this->assertArrayHasKey('1', $class->getProtectedVar('preparedSQL'));
+        $this->assertArrayHasKey('2', $class->getProtectedVar('preparedSQL'));
+
+        $this->assertEquals('INSERT INTO `table_1` (`key`) VALUES (:value1);', $class->getProtectedVar('preparedSQL')[0]);
+        $this->assertEquals('INSERT INTO `table_1` (`key`) VALUES (:value1);', $class->getProtectedVar('preparedSQL')[1]);
+        $this->assertEquals('INSERT INTO `table_1` (`key`) VALUES (:value1);', $class->getProtectedVar('preparedSQL')[2]);
     }
 
 }
